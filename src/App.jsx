af413f9f47bc5092d3eab4d621eebea7095a8b34
woranan hernandez <lovelywomen.2254@gmail.com>
2023-09-25 18:29:25 -0700

    adjust app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Header from "./components/pages/Header";
 import ProductsData from "./components/ProductsData";
 import { Routes, Route } from "react-router-dom";
-import Login from "./components/pages/Login";
+import Login from "./components/pages/Logining";
 import { useState } from "react";
 import Footer from "./components/pages/Footer";
 import "./App.css";
 import SingleProduct from "./components/SingleProduct";
-import { CartProvider } from "./context/cartcontext";
+import { CartProvider } from "./context/CartContext";
 import Register from "./components/pages/Register";
 
 function App() {
